da6b8795ed5e0655177c0a82969144b28e5380bd
Aguda <agudaoluwanifemi@gmail.com>
2022-09-30 00:49:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 
 function App() {
+  const [value, onChange] = useState(new Date());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{color:'white'}}>
+        {value.toString()}
+      </div>
+      <Calendar onChange={onChange} value={value} minDate={new Date("01-01-2000")} maxDate={new Date("12-31-2030")} className="cal" />
     </div>
+    
   );
 }
 
